171ec8ec0bb2a5ab8ebbf5848c199770f7103008
choumame <pyonazuki@gmail.com>
2023-08-09 16:56:59 +0900

    ログイン画面が汚いのを修正、ログイン失敗時にエラーを表示

--- src/resources/js/components/Login.vue
+++ src/resources/js/components/Login.vue
@@ -20,6 +20,7 @@ export default {
       result: '',
       error: '',
       showPassword: false,
+      errorMessage: '',
     };
   },
   created() {
@@ -49,10 +50,23 @@ export default {
           case 'admin':
             router.push('/admin');
             break;
-        }
+        } 
+      } else {
+          this.errorMessage = 'ログインに失敗しました。再度QRコードを読み取りログインできるかご確認ください。\nそれでもログインできない場合はユーザー名とパスワードを入力してログインをお願い致します。'
       }
     },
    async loginByForm(user: string, password: string) {
+    if(user == '' && password == '') {
+      this.errorMessage = 'ユーザー名・パスワードをご入力ください。';
+      return;
+    } else if(user == '') {
+      this.errorMessage = 'ユーザー名をご入力ください。'
+      return;
+    } else if(password == ''){
+      this.errorMessage = 'パスワードをご入力ください。'
+      return;
+    }
+
       try {
         await http.get('/sanctum/csrf-cookie');
 
@@ -74,6 +88,7 @@ export default {
       }
     }
     } catch (err) {
+      this.errorMessage = 'ログインに失敗しました。\nユーザー名とパスワードをお確かめの上再度ログインをお願い致します。'
       console.error('Login error:', err)
     }
   },
@@ -86,30 +101,32 @@ export default {
     <v-app-bar color="blue">
       <v-app-bar-title>ログイン</v-app-bar-title>
     </v-app-bar>
-    <v-card width="400px" class="mx-auto mt-5">
+    <v-container style="padding-top: 10%;">
+      <v-card width="400px" class="mx-auto mt-5">
       <v-card-title>
-        <h1 class="display-1">ログイン</h1>
+        <h1 class="display-2">ログイン</h1>
       </v-card-title>
-    <v-card-text>
-      <p>ご利用いただくにはログインが必要です。<br>お部屋にございますユーザー名とパスワードをお確かめの上、ログインをお願い致します。</p>
-    </v-card-text>
-    <v-card-text>
-      <v-form>
-        <v-text-field v-model="user" prepend-icon="mdi-account-key" Label="ユーザー名" />
-        <v-text-field 
-          v-model="password"
-          v-bind:type="showPassword ? 'text' : 'password'"
-          @click:append="showPassword = !showPassword"
-          prepend-icon="mdi-lock" 
-          append-icon="mdi-eye-off"
-          Label="パスワード" />
-      </v-form>
-      <v-checkbox></v-checkbox>
-      <v-card-actions>
-        <v-btn @click="loginByForm(user, password)" class="info" color="primary">ログイン</v-btn>
-      </v-card-actions>
-    </v-card-text>
-  </v-card>
+      <v-card-text>
+        <p>ご利用いただくにはログインが必要です。<br>お部屋にございますユーザー名とパスワードをお確かめの上、ログインをお願い致します。</p>
+      </v-card-text>
+      <v-card-text>
+        <v-form>
+          <v-text-field v-model="user" prepend-icon="mdi-account-key" Label="ユーザー名" />
+            <v-text-field 
+              v-model="password"
+              v-bind:type="showPassword ? 'text' : 'password'"
+              @click:append="showPassword = !showPassword"
+              prepend-icon="mdi-lock" 
+              append-icon="mdi-eye-off"
+              Label="パスワード" />
+        </v-form>
+        <v-card-text color="red" v-if="errorMessage.length != 0" style="white-space:pre-wrap; word-wrap:break-word;">{{ errorMessage }}</v-card-text>
+          <v-card-actions>
+            <v-btn color="blue" @click="loginByForm(user, password)" class="info">ログイン</v-btn>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-container>
   </v-app> 
 
 </template>
